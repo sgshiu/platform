2adce368c24959b82926687ab175421e0aeaa8b9
sgshiu <sgshiu@hotmail.com>
2015-05-19 03:40:01 +0800

    === version = 0.2.20150519 ===
    1. finish list insert test (dummy)
    2. finish list struct

--- list.h
+++ list.h
@@ -1,9 +1,21 @@
 #ifndef GAWIN_LIST_H
 #define GAWIN_LIST_H
 
+struct list
+{
+	struct list *prev;
+	struct list *next;
+};
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define list_init(h) (h)->prev = h; (h)->next = h;
+
+#define list_insert_next(h, p) printf("not implement")
+#define list_insert_prev(h, p) printf("not implement")
+#define list_entity(h) NULL
 
 #ifdef __cplusplus
 }
--- main.c
+++ main.c
@@ -1,7 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <assert.h>
+
+#include "list.h"
+
+struct my_list
+{
+	int val;
+	struct list h;
+};
 
 int main(int argc, char *argv[])
 {
+	struct my_list h, h1, h2, h3;
+	struct my_list *p = NULL;
+
+	h.val = 0;
+	h1.val = 1;
+	h2.val = 2;
+	h3.val = 3;
+	
+	list_init(&h.h);
+	list_insert_prev(&h.h, &h1.h);
+	list_insert_next(&h.h, &h2.h);
+	list_insert_next(&h.h, &h3.h);
+	p = list_entity(list_next(&h.h));
+	assert(p->val == 2);
+	p = list_entity(list_next(&p->h));
+	assert(p->val == 3);
+	p = list_entity(list_next(&p->h));
+	assert(p->val == 1);
+	
 	return 0;
 }
